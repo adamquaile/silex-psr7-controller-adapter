f33aae5524fe9dcee6621bf70604ec55a318b29b
Gabor Pihaj <gabor.pihaj@gmail.com>
2016-02-06 21:19:20 +0000

    Use server request interface in the test controller

--- test/Lib/DummyController.php
+++ test/Lib/DummyController.php
@@ -1,17 +1,17 @@
 <?php
 namespace SilexPsr7Test\Lib;
 
-use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response;
 
 class DummyController
 {
-    public function testAction(RequestInterface $request)
+    public function testAction(ServerRequestInterface $request)
     {
         return new Response();
     }
 
-    public function  __invoke(RequestInterface $request)
+    public function __invoke(ServerRequestInterface $request)
     {
         return new Response();
     }
